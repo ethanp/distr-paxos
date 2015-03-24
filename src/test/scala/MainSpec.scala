4b6994e1c5b7a8f9c46aeb0d2c7469173ff9fcdb
ethanp <ethanp@utexas.edu>
2015-03-24 13:57:53 -0500

    first commit

---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,15 @@
+import org.scalatest.WordSpec
+
+/**
+ * Ethan Petuchowski
+ * 3/24/15
+ */
+class MainSpec extends WordSpec {
+  "Main" when {
+    "exists" should {
+      "have true" in {
+        assert(true)
+      }
+    }
+  }
+}
